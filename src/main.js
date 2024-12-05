1ba5792a89cea49c0c5546c2268ac12b975ba4fe
sergs1305 <sergs13@gmail.com>
2024-12-05 16:26:54 +0300

    Время разбивать камни (компоненты)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import {render} from './render.js';
+import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filters-view.js';
 import MainPresenter from './presenter/presenter.js';
 
-const tripControlsFilters = document.querySelector('.trip-controls__filters');
-const tripEvents = document.querySelector('.trip-events');
-const mainPresenter = new MainPresenter({boardContainer: tripEvents});
+const tripMain = document.querySelector('.trip-main');
+const tripControlsFilters = tripMain.querySelector('.trip-controls__filters');
+//const tripEvents = document.querySelector('.trip-events');
+//const mainPresenter = new MainPresenter({boardContainer: tripEvents});
+const mainPresenter = new MainPresenter();
 
+render(new TripInfoView(), tripMain, 'AFTERBEGIN');
 render(new FilterView(), tripControlsFilters);
 
 mainPresenter.init();
--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,28 +1,33 @@
 import {render} from '../render.js';
-import SortingView from './view/sorting-view.js';
-import CreationFormView from './view/creation-form-view.js';
-import WaypointView from './view/waypoint-view.js';
+import SortingView from '../view/sorting-view.js';
+//import CreationFormView from '../view/creation-form-view.js';
+import EditingFormView from '../view/editing-form-view.js';
+import ListView from '../view/list-view.js';
+import WaypointView from '../view/waypoint-view.js';
 
 const tripEvents = document.querySelector('.trip-events');
-
-render(new SortingView(), tripEvents);
-render(new CreationFormView(), tripEvents);
 
 export default class Presenter {
   // boardComponent = new BoardView();
   // taskListComponent = new TaskListView();
 
-  constructor({boardContainer}) {
-    this.boardContainer = boardContainer;
-  }
+  // constructor({boardContainer}) {
+  //   this.boardContainer = boardContainer;
+  // }
 
   init() {
+    render(new SortingView(), tripEvents);
+    render(new ListView(), tripEvents); // отрисовываем тэг ul - контейнер списка точек маршрута
+    const tripEventsList = tripEvents.querySelector('.trip-events__list');
+    //render(new CreationFormView(), tripEventsList);
+    render(new EditingFormView(), tripEventsList);
+
     // render(this.boardComponent, this.boardContainer);
     // render(new SortView(), this.boardComponent.getElement());
     // render(this.taskListComponent, this.boardComponent.getElement());
     // render(new TaskEditView(), this.taskListComponent.getElement());
     for (let i = 0; i < 3; i++) {
-      render(new WaypointView(), this.taskListComponent.getElement());
+      render(new WaypointView(), tripEventsList);
     }
     // render(new LoadMoreButtonView(), this.boardComponent.getElement());
   }
